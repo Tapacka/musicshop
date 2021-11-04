800f8cdd75174190dc4fb3a71499ea2610385db9
Taras Tsoin <tsoint@mail.ru>
2021-11-04 17:10:19 +0300

    2

--- music.sql
+++ music.sql
@@ -12,18 +12,32 @@ id_genre serial primary key,
 CREATE TABLE ARTISTS (
 ARTIST_ID serial primary key,
  artist_name varchar(100) not null,
- id_genre integer not null references genres(id_genre));
+ );
+
+CREATE TABLE genre-artist (
+ id_genre integer references genres(id_genre),
+ ARTIST_ID integer references ARTISTS(ARTIST_ID), 
+ constraint AG primary key(id_genre, ARTIST_ID));
 
 CREATE TABLE albums (
 album_id serial primary key,
  album_name varchar(100) not null,
- artist_id integer not null references artists(artist_id), 
-year integer);
+ year integer);
+
+CREATE TABLE artist-album (
+ album_id integer references albums(album_id),
+ ARTIST_ID integer references ARTISTS(ARTIST_ID), 
+ constraint AA primary key(album_id, ARTIST_ID));
 
 CREATE TABLE Collection (id_collection serial primary key, collection_name varchar(100) not null, year integer)
 
 CREATE TABLE tracks (id_track serial primary key,
- album_id integer not null references albums(album_id), track_name varchar(100),
- track_length varchar(40),
-id_collection integer references Collection(id_collection) not null
+ album_id integer not null references albums(album_id),
+ track_name varchar(100),
+ track_length integer,
  );
+
+CREATE TABLE Collection-tracks (
+ id_collection integer references Collection(id_collection),
+ id_track integer references tracks(id_track), 
+ constraint CT primary key(id_collection, id_track));
