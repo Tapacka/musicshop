14922e74a247ceb8733b9b690658eec7507c67e5
Taras Tsoin <tsoint@mail.ru>
2021-11-11 23:18:47 +0300

    1

--- create.sql
+++ create.sql
@@ -11,10 +11,10 @@ id_genre serial primary key,
 
 CREATE TABLE ARTISTS (
 ARTIST_ID serial primary key,
- artist_name varchar(100) not null,
+ artist_name varchar(100) not null
  );
 
-CREATE TABLE genre-artist (
+CREATE TABLE genre_artist (
  id_genre integer references genres(id_genre),
  ARTIST_ID integer references ARTISTS(ARTIST_ID), 
  constraint AG primary key(id_genre, ARTIST_ID));
@@ -24,12 +24,12 @@ album_id serial primary key,
  album_name varchar(100) not null,
  year integer);
 
-CREATE TABLE artist-album (
+CREATE TABLE artist_album (
  album_id integer references albums(album_id),
  ARTIST_ID integer references ARTISTS(ARTIST_ID), 
  constraint AA primary key(album_id, ARTIST_ID));
 
-CREATE TABLE Collection (id_collection serial primary key, collection_name varchar(100) not null, year integer)
+CREATE TABLE colection (id_collection serial primary key, collection_name varchar(100) not null, year integer);
 
 CREATE TABLE tracks (id_track serial primary key,
  album_id integer references albums(album_id),
@@ -37,7 +37,7 @@ CREATE TABLE tracks (id_track serial primary key,
  track_length integer
  );
 
-CREATE TABLE Collection-tracks (
- id_collection integer references Collection(id_collection),
+CREATE TABLE colection_tracks (
+ id_collection integer references colection(id_collection),
  id_track integer references tracks(id_track), 
  constraint CT primary key(id_collection, id_track));
--- select.sql
+++ select.sql
@@ -1,6 +1,6 @@
 SELECT album_name, year FROM albums WHERE year = 2018;
-SELECT track_name, track_length FROM tracks ORDER BY track_length LIMIT 1;
+SELECT track_name, track_length FROM tracks ORDER BY track_length DESC LIMIT 1;
 SELECT track_name FROM tracks WHERE track_length >= 210;
-SELECT collection_name FROM Collection WHERE year BETWEEN 2018 AND 2020;
-SELECT * FROM ARTISTS WHERE artist_name LIKE '_%_';
-SELECT track_name FROM tracks WHERE track_name LIKE '%my%';
+SELECT collection_name FROM colection WHERE year BETWEEN 2018 AND 2020;
+SELECT * FROM ARTISTS WHERE artist_name NOT LIKE '%% %%';
+SELECT track_name FROM tracks WHERE track_name LIKE '%My%';
